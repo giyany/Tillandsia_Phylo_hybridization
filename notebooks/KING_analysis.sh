08a5509376140893a3730aa15deb3a90cefcd09d
giyany <37343437+giyany@users.noreply.github.com>
2024-10-28 18:54:23 +0100

    Update KING_analysis.sh

--- notebooks/KING_analysis.sh
+++ notebooks/KING_analysis.sh
@@ -4,15 +4,22 @@
 #SBATCH -N 2
 #SBATCH --ntasks-per-node=16
 #SBATCH --ntasks-per-core=1
-#SBATCH -p mem_0096
-#SBATCH --qos p71400_0096
 
-source /home/fs71400/yardeni/.bashrc 
+source .bashrc 
 conda init bash
 conda activate KING-env
 
 #go to wd
 
-cd /gpfs/data/fs71400/yardeni/WGS/vcf;
+cd /vcf;
 
-plink2 --vcf Variants.AllTillandsias.allChr.WGS.raw.full.vcf --make-bed --allow-extra-chr --max-alleles 2 --out Variants.AllTilandsias.raw.full
+plink2 --vcf Variants.AllTillandsias.allChr.WGS.raw.full.vcf --make-bed --allow-extra-chr --max-alleles 2 --set-all-var-ids @:# --out Variants.AllTillandsias.3bpindel.0.2miss.plink
+
+mv Variants.AllTillandsias.3bpindel.0.2miss.plink.* ../KING_analysis/;
+cd ../KING_analysis/
+mv Variants.AllTillandsias.3bpindel.0.2miss.plink.bim Variants.AllTillandsias.3bpindel.0.2miss.plink.bim.old;
+
+#Change chr names for king
+sed -e 's/Scaffold\_//g' Variants.AllTillandsias.3bpindel.0.2miss.plink.bim.old > Variants.AllTillandsias.3bpindel.0.2miss.plink.bim;
+
+king -b Variants.AllTillandsias.3bpindel.0.2miss.plink.bed --kinship
